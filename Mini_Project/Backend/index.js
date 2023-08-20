e610777ac810342a14d4e9cb89184273a5be7f22
rohitnayal12 <rohitnayalkld@gmail.com>
2023-08-20 14:24:52 +0530

    added gitignore

--- Mini_Project/Backend/index.js
+++ Mini_Project/Backend/index.js
@@ -4,10 +4,42 @@ const { userRouter } = require('./routes/userRoute')
 const { noteRouter } = require('./routes/noteRoute')
 const cors=require("cors")
 
+const swaggerJSdoc=require("swagger-jsdoc")
+const swaggerUI=require("swagger-ui-express")
+
 const app=express()
 
 app.use(cors())
 app.use(express.json())
+
+//configuration for swaggerJSdoc
+ 
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Making Notes',
+        version: '1.0.0',
+      },
+      servers:[
+        {
+          url:"http://localhost:8080"  
+        }
+    ]
+    },
+    
+    apis: ['./routes/*.js'], // files containing annotations as above
+  };
+
+  //openAPI specs 
+
+  const openapiSpec = swaggerJSdoc(options)
+
+  //build the swaggerUI with the help of openAPIspec
+app.use("/docs",swaggerUI.serve,swaggerUI.setup(openapiSpec))
+
+
+
 app.use("/users",userRouter)
 app.use("/notes",noteRouter)
 
--- Mini_Project/Backend/routes/noteRoute.js
+++ Mini_Project/Backend/routes/noteRoute.js
@@ -4,91 +4,201 @@ const { auth } = require("../middlewares/auth.middleware");
 
 const noteRouter = express.Router();
 
-//add notes
-noteRouter.post("/create",auth,async(req,res)=>{
 
-    try {
-        note=await NoteModel(req.body)
-        await note.save()
-        return res.status(200).send({"msg":"A new note has been created."}) 
-    } catch (error) {
-        res.send({"err":error})
-    }
-    
-})
+/**
+ * @swagger
+ * tags:
+ *   name: Notes
+ *   description: API routes related to Notes
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /notes/create:
+ *    post:
+ *      summary: Create a new note
+ *      tags: [Notes]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                title:
+ *                  type: string
+ *                content:
+ *                  type: string
+ *      responses:
+ *        200:
+ *          description: A new note has been created
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /notes:
+ *    get:
+ *      summary: Get all notes for the authenticated user
+ *      tags: [Notes]
+ *      responses:
+ *        200:
+ *          description: List of notes
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Note'
+ *        500:
+ *          description: Internal server error
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /notes/update/{noteID}:
+ *    patch:
+ *      summary: Update a note by noteID
+ *      tags: [Notes]
+ *      parameters:
+ *        - name: noteID
+ *          in: path
+ *          required: true
+ *          schema:
+ *            type: string
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/NoteUpdate'
+ *      responses:
+ *        200:
+ *          description: Note updated successfully
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /notes/delete/{noteID}:
+ *    delete:
+ *      summary: Delete a note by noteID
+ *      tags: [Notes]
+ *      parameters:
+ *        - name: noteID
+ *          in: path
+ *          required: true
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: Note deleted successfully
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Note:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The auto-generated ID of the note
+ *         title:
+ *           type: string
+ *           description: The title of the note
+ *         content:
+ *           type: string
+ *           description: The content of the note
+ *         userId:
+ *           type: string
+ *           description: The user ID associated with the note
+ *     NoteUpdate:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The updated title of the note
+ *         content:
+ *           type: string
+ *           description: The updated content of the note
+ *       required:
+ *         - title
+ *         - content
+ */
+
+
+
+
+//add notes
+
+noteRouter.post("/create", auth, async (req, res) => {
+  try {
+    note = await NoteModel(req.body);
+    await note.save();
+    return res.status(200).send({ msg: "A new note has been created." });
+  } catch (error) {
+    res.send({ err: error });
+  }
+});
 
 
 //get notes
-noteRouter.get("/",auth,async(req,res)=>{
+noteRouter.get("/", auth, async (req, res) => {
+  try {
+    const notes = await NoteModel.find({ userId: req.body.userId });
+    return res.status(200).send(notes);
+  } catch (error) {
+    res.send({ err: err });
+  }
+});
 
-    try {
-        const notes=  await NoteModel.find({userId:req.body.userId})
-        return res.status(200).send(notes) 
-    } catch (error) {
-        res.send({"err":err})
-    }
-    
-})
 
 
 //update note with the help of noteID
-noteRouter.patch("/update/:noteID",auth,async(req,res)=>{
-const {noteID}=req.params
-  note =  await NoteModel.findOne({_id:noteID})
-  console.log(note)
+noteRouter.patch("/update/:noteID", auth, async (req, res) => {
+  const { noteID } = req.params;
+  note = await NoteModel.findOne({ _id: noteID });
+  console.log(note);
 
-console.log(req.body.userId)
-console.log(note.userId)
-    try {
-        if(req.body.userId!==note.userId){
-           return res.send({"msg":"You are not authorized!!!!!!!"})
-        }
-        else{
-            note =await NoteModel.findByIdAndUpdate({_id:noteID},req.body)
-            return res.status(200).send(`Note with noteID ${noteID} has been updated.`) 
-        }
-        
-    } catch (error) {
-        res.send({"err":error})
+  console.log(req.body.userId);
+  console.log(note.userId);
+  try {
+    if (req.body.userId !== note.userId) {
+      return res.send({ msg: "You are not authorized!!!!!!!" });
+    } else {
+      note = await NoteModel.findByIdAndUpdate({ _id: noteID }, req.body);
+      return res
+        .status(200)
+        .send(`Note with noteID ${noteID} has been updated.`);
     }
-    
-})
-
+  } catch (error) {
+    res.send({ err: error });
+  }
+});
 
 //delete note
-noteRouter.delete("/delete/:noteID",auth,async(req,res)=>{
-    const {noteID}=req.params
-    note =await NoteModel.findOne({_id:noteID})
-        try {
-            if(req.body.userId!==note.userId){
-                return res.send({"msg":"You are not authorized!!!!!!!"})
-             }
-             else{
-                note =await NoteModel.findByIdAndDelete({_id:noteID})
-        
-                return res.status(200).send(`Note with noteID ${noteID} has been deleted.`)  
-            }
-           
-        } catch (error) {
-            res.send({"err":err})
-        }
-        
-    })
+noteRouter.delete("/delete/:noteID", auth, async (req, res) => {
+  const { noteID } = req.params;
+  note = await NoteModel.findOne({ _id: noteID });
+  try {
+    if (req.body.userId !== note.userId) {
+      return res.send({ msg: "You are not authorized!!!!!!!" });
+    } else {
+      note = await NoteModel.findByIdAndDelete({ _id: noteID });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+      return res
+        .status(200)
+        .send(`Note with noteID ${noteID} has been deleted.`);
+    }
+  } catch (error) {
+    res.send({ err: err });
+  }
+});
 
 module.exports = {
-    noteRouter,
-  };
+  noteRouter,
+};
